9763b943953a79216bf3a75cda33e87a4fad936e
Gaurav Singh <gauravnumber@gmail.com>
2021-03-26 12:33:18 +0530

    add part 1c and rename part0

---
+++ part1/part 1b/src/Content.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Part from './Part'
+
+const Content = (props) => {
+    return (
+         <Part course={props.course} />
+    )
+}
+
+export default Content
---
+++ part1/part 1c/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const Display = ({ counter }) =>   <div>{counter}</div> 
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="Plus" />
+      <Button handleClick={setToZero} text="Zero" />
+      <Button handleClick={decreaseByOne} text="Minus" />
+    </div>
+  )
+}
+
+export default App
