d418b89415a1f88d9c9143fac43a4fb4a423979d
Gaurav Singh <gauravnumber@gmail.com>
2021-04-20 14:12:55 +0530

    exercise 4.7 completed

---
+++ part 4/part 4a/exercise/exercise 4.7/blogs/backend/utils/list_helper.js
@@ -0,0 +1,73 @@
+const totalLikes = blogs => {
+  return blogs.reduce((sum, item) => sum + item.likes, 0)
+}
+
+const favoriteBlog = blogs => {
+  let mostLiked = 0
+  let mostLikedId
+  blogs.forEach(item => {
+    if (item.likes > mostLiked) {
+      mostLiked = item.likes
+      mostLikedId = item._id
+    }
+  })
+
+  const blog = blogs.find(blog => blog._id === mostLikedId)
+
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes
+  }
+}
+
+const mostBlogs = blogs => {
+  const arrBlogName = blogs.map(blog => blog.author)
+
+  return arrBlogName
+}
+
+const mostLikes = blogs => {
+  // const arrBlogLikes = blogs.map(blog => blog.likes)
+  let mostLiked = 0
+  let totalNumberOfLikes = 0
+  let mostLikedAuthor
+
+  blogs.forEach(blog => {
+    if (blog.likes > mostLiked) {
+      mostLiked = blog.likes
+      mostLikedAuthor = blog.author      
+      // console.log('mostLikedAuthor', mostLikedAuthor);
+    }
+  })
+
+
+  // let totalNumberOfLikes = blogs.reduce((sum, item) => {
+  //   if (item.author === mostLikedAuthor) {
+  //     console.log('item.likes', item.likes);
+  //     console.log('sum', sum);
+  //     return sum + item.likes
+  //   }
+  // }, 0)
+
+  blogs.forEach(blog => {
+    if (blog.author === mostLikedAuthor) {
+      totalNumberOfLikes += blog.likes
+    }
+  })
+
+  // console.log('totalNumberOfLikes', totalNumberOfLikes);
+  
+
+  return {
+    author: mostLikedAuthor,
+    likes: totalNumberOfLikes
+  }
+}
+
+module.exports = {
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
