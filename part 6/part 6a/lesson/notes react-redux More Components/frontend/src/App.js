fb04d2e781fa146f4390e7c91ab66f9921c3e5ac
Gaurav Singh <gauravnumber@gmail.com>
2021-05-18 11:04:15 +0530

    fs

---
+++ part 6/part 6a/lesson/notes react-redux More Components/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewNote from './NewNote'
+import Notes from './Notes'
+
+const App = () => {
+  return (
+    <div>
+      <NewNote />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
