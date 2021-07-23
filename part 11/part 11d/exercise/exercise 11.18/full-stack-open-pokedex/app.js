39acaa5ea99263a0beb750feedf072cdaf93b334
Gaurav Singh <gauravnumber@gmail.com>
2021-07-23 21:04:49 +0530

    fs

---
+++ part 11/part 11d/exercise/exercise 11.18/full-stack-open-pokedex/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('23')
+})
+
+
+app.listen(PORT)
